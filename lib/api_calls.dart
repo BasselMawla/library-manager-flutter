a4c99b2316af82965be3e04b2f4bf5d3762d85bc
Bassel Ballout El Mawla <balloutelb@coventry.ac.uk>
2022-03-19 18:26:56 +0000

    Moved BooksList to its own file

--- lib/api_calls.dart
+++ lib/api_calls.dart
@@ -3,7 +3,8 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 const baseUrl = 'https://mobile-library-api.herokuapp.com/';
-const token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2NDc2MTY1MzIsInN1YiI6IjI4IiwiaXNzIjoiaHR0cHM6Ly9tb2JpbGUtbGlicmFyeS1hcGkuaGVyb2t1YXBwLmNvbS8ifQ.Y9D_0F7GyNv1fbUvDb8QSfkBQU4qupRODntNnKRuuwo';
+const token =
+    'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2NDc2MTY1MzIsInN1YiI6IjI4IiwiaXNzIjoiaHR0cHM6Ly9tb2JpbGUtbGlicmFyeS1hcGkuaGVyb2t1YXBwLmNvbS8ifQ.Y9D_0F7GyNv1fbUvDb8QSfkBQU4qupRODntNnKRuuwo';
 
 Future<Map> getAllBooks() async {
   var url = Uri.parse(baseUrl + 'books');
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
+import 'package:cem7052_library/routes/books_list.dart';
 import 'package:flutter/material.dart';
-
-import 'api_calls.dart';
 
 void main() {
   runApp(const MyApp());
@@ -49,84 +48,15 @@ class MyApp extends StatelessWidget {
         ),
         body: TabBarView(
           children: [
+            // Books tab
             const BooksList(),
+            // Students tab, if librarian
             if (isLibrarian) const Center(child: Text('STUDENTS')),
+            // Profile tab
             const Center(child: Text('PROFILE')),
           ],
         ),
       ),
-    );
-  }
-}
-
-class BooksList extends StatefulWidget {
-  const BooksList({Key? key}) : super(key: key);
-
-  @override
-  State<BooksList> createState() => _BooksListState();
-}
-
-class _BooksListState extends State<BooksList> {
-  late Future<Map> allBooks;
-  final _biggerFont = const TextStyle(fontSize: 18.0);
-
-  @override
-  void initState() {
-    super.initState();
-    allBooks = getAllBooks();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: FutureBuilder<Map>(
-        future: allBooks,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            print(snapshot.data!);
-            return buildBookListView(snapshot.data!);
-          } else if (snapshot.hasError) {
-            print('ERROR');
-            print(snapshot.error);
-            return Text('${snapshot.error}');
-          }
-
-          // Show a loading spinner.
-          return const CircularProgressIndicator();
-        },
-      ),
-    );
-  }
-
-  ListView buildBookListView(Map booksMap) {
-    List booksList = booksMap['books'];
-    return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemCount: booksMap['books'].length * 2,
-      itemBuilder: (context, i) {
-        if (i.isOdd) {
-          return const Divider(
-            thickness: 1.0,
-          );
-        }
-
-        final index = i ~/ 2; // To ignore dividers
-        Map currentBook = booksList[index];
-        String bookInfo = currentBook['title'].toString() +
-            ' by ' +
-            currentBook['author'].toString();
-        String stockInfo = ' Stock: ' + currentBook['stock'].toString();
-        return ListTile(
-          leading: const Icon(Icons.book),
-          title: Text(
-            bookInfo,
-            style: _biggerFont,
-          ),
-          trailing: Text(
-            stockInfo,
-          ),
-        );
-      },
     );
   }
 }
